9c690395a0630bc91689b923801c8f86191e9028
l3056762892 <l3056762892@gmail.com>
2022-06-03 22:05:36 +0800

    init commit

--- gisweb/gisweb/urls.py
+++ gisweb/gisweb/urls.py
@@ -1,27 +1,27 @@
 from django.contrib import admin
-from django.conf.urls import url
+from django.urls import include, re_path
 from nccu_gis_app.views import index, map, add, login, logout, profileUpdate, error, modify, blog, user_blog, vrtest
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
-    url(r'^$',map),
-    url(r'^admin/', admin.site.urls),
-    url(r'^index/$', index),
-    url(r'^map/$', map),
-    url(r'^add/$',add),
-    url(r'^modify/$', modify),
-    url(r'^login/$',login),
-    url(r'^logout/$',logout),
-    url(r'^profileUpdate/$',profileUpdate),
-    url(r'^error/$',error),
-    url(r'^blog/$',blog),
-    url(r'^modify/(\w+)/$',modify),
-    url(r'^modify/(\w+)/(\w+)/$',modify),
-    url(r'^user_blog/$', user_blog),
-    url(r'^vrtest/$',vrtest),
-    url(r'^vrtest/(\w+)/$',vrtest),
+    re_path(r'^$',map),
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^index/$', index),
+    re_path(r'^map/$', map),
+    re_path(r'^add/$',add),
+    re_path(r'^modify/$', modify),
+    re_path(r'^login/$',login),
+    re_path(r'^logout/$',logout),
+    re_path(r'^profileUpdate/$',profileUpdate),
+    re_path(r'^error/$',error),
+    re_path(r'^blog/$',blog),
+    re_path(r'^modify/(\w+)/$',modify),
+    re_path(r'^modify/(\w+)/(\w+)/$',modify),
+    re_path(r'^user_blog/$', user_blog),
+    re_path(r'^vrtest/$',vrtest),
+    re_path(r'^vrtest/(\w+)/$',vrtest),
 ]
 
 
